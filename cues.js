440b33be3a8c40a705e5609afc22979d3584af36
rafgim2 <160648192+rafgim2@users.noreply.github.com>
2026-01-22 23:04:34 +0100

    Update cues.js

--- cues.js
+++ cues.js
@@ -4,11 +4,11 @@ export const VIDEO_URL = "https://pub-aa6ee859836d478ca4ac85eb9f71f0c3.r2.dev/br
 // Cues (incluye título con type:"title")
 export const CUES = [
   // ✅ TÍTULO (ajusta el tiempo a cuando quieras que aparezca)
-  { t: 2.0, type: "title", text: ["Broken note "] },
+  { t: 0.0, type: "title", text: ["Broken note "] },
 
   // ✅ TEXTO NORMAL
 
- { t: 15.0, type: "text", text: ["Incompleto y roto,", "con aristas desiguales que parecen no cuadrar…"] },
+ { t: 10.0, type: "text", text: ["Incompleto y roto,", "con aristas desiguales que parecen no cuadrar…"] },
   { t: 25.0, type: "text", text: ["desapercibido, invisible,", "incomprendido en su complejidad…"] },
   { t: 40.0, type: "text", text: ["Incompleto y roto,", "pero destinado a encajar en algún lugar…"] },
   { t: 55.0, type: "text", text: ["A veces distante,", "anhelando la perfección de la inmensidad…"] },
@@ -17,6 +17,7 @@ export const CUES = [
   { t: 115.0, type: "text", text: ["Formando parte de un todo,", "aunque sus grietas griten su fragilidad…"] },
   { t: 130.0, type: "text", text: ["Incompleto y roto, pero un enigma", "y un susurro para los que se atrevan a escuchar…"] },
 ];
+
 
 
 
